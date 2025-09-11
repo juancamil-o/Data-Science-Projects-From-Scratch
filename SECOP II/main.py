e7b58dab6eb117a4e21790e92061d3900f94dde4
Juan Pimienta <jpimienta@agenciaatenea.gov.co>
2025-09-11 10:09:13 -0500

    change to 2 days back

--- SECOP II/main.py
+++ SECOP II/main.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     numeroDiasAtras = 1
     if extraerContratosDelDia(numeroDiasAtras) > 0:
         transformar_datos_del_dia(
-            dias_retro=1,
+            dias_retro=2,
             json_path="datosDeHoy.json",
             out_dir="s3://proyecto-secop/processed/",   # <-- S3
             out_format="parquet",                   # parquet recomendado
